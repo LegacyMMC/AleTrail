026b8ca526b5599f4342a335e7dd9b4205ad178e
LegacyMMC <MMCApplications@outlook.com>
2024-04-13 13:00:24 +0100

    Clean up and colour fix

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
     const Page1(),
     const Page2(),
     const Page3(),
-    const Page4(),
-    const Page5(),
+    const Page4()
   ];
 
   @override
@@ -138,57 +137,47 @@ class _MyHomePageState extends State<MyHomePage> {
         kIconSize: 24.0,
       )
           : null,
+      bottomSheet: Container(height: 0),
     );
   }
 }
 
 class Page1 extends StatelessWidget {
-  const Page1({Key? key}) : super(key: key);
+  const Page1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.yellow, child: const Center(child: Text('Page 1')));
+        color: Colors.grey, child: const Center(child: Text('Page 1')));
   }
 }
 
 class Page2 extends StatelessWidget {
-  const Page2({Key? key}) : super(key: key);
+  const Page2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.green, child: const Center(child: Text('Page 2')));
+        color: Colors.grey, child: const Center(child: Text('Page 2')));
   }
 }
 
 class Page3 extends StatelessWidget {
-  const Page3({Key? key}) : super(key: key);
+  const Page3({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.red, child: const Center(child: Text('Page 3')));
+        color: Colors.grey, child: const Center(child: Text('Page 3')));
   }
 }
 
 class Page4 extends StatelessWidget {
-  const Page4({Key? key}) : super(key: key);
+  const Page4({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.blue, child: const Center(child: Text('Page 4')));
-  }
-}
-
-class Page5 extends StatelessWidget {
-  const Page5({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.lightGreenAccent,
-        child: const Center(child: Text('Page 5')));
+        color: Colors.grey, child: const Center(child: Text('Page 4')));
   }
 }
